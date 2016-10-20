b933d5c83ff5e67bb686e2d2cb21d26a2d642c35
Alex Van Camp <vancamp.alex@gmail.com>
2016-10-19 21:35:58 -0500

    refactor: add an `init` method which returns a promise

--- test/manager.spec.js
+++ test/manager.spec.js
@@ -1,13 +1,13 @@
 'use strict';
 
-var fs = require('fs');
-var path = require('path');
-var rimraf = require('rimraf');
-var wrench = require('wrench');
-var chai = require('chai');
-var expect = chai.expect;
-var assert = chai.assert;
-var Logger = require('@nodecg/logger')({console: {enabled: true}});
+const fs = require('fs');
+const path = require('path');
+const rimraf = require('rimraf');
+const wrench = require('wrench');
+const chai = require('chai');
+const expect = chai.expect;
+const assert = chai.assert;
+const Logger = require('@nodecg/logger')({console: {enabled: true}});
 
 before(function (done) {
 	this.timeout(30000);
@@ -18,32 +18,32 @@ before(function (done) {
 
 	wrench.copyDirSyncRecursive('test/fixtures', '_workingTest', {forceDelete: true});
 
-	this.bundleManager = require('../index.js')('_workingTest', '0.7.0', {}, Logger);
-	this.bundleManager.on('allLoaded', function () {
+	this.bundleManager = require('../index.js');
+	this.bundleManager.init('_workingTest', '0.7.0', {}, Logger).then(() => {
 		// Needs a little extra wait time for some reason.
 		// Without this, tests randomly fail.
-		setTimeout(function () {
+		setTimeout(() => {
 			done();
 		}, 100);
 	});
 });
 
-describe('loader', function () {
+describe('loader', () => {
 	it('should detect and load bundle configuration files', function () {
-		var bundle = this.bundleManager.find('config-test');
+		const bundle = this.bundleManager.find('config-test');
 		expect(bundle.config).to.deep.equal({bundleConfig: true});
 	});
 
 	it('should not load bundles with a non-satisfactory nodecg.compatibleRange', function () {
-		var bundle = this.bundleManager.find('incompatible-range');
+		const bundle = this.bundleManager.find('incompatible-range');
 		assert.isUndefined(bundle);
 	});
 });
 
-describe('watcher', function () {
+describe('watcher', () => {
 	it('should emit a change event when the manifest file changes', function (done) {
-		var manifest = JSON.parse(fs.readFileSync('_workingTest/bundles/change-manifest/package.json'));
-		this.bundleManager.once('bundleChanged', function (bundle) {
+		const manifest = JSON.parse(fs.readFileSync('_workingTest/bundles/change-manifest/package.json'));
+		this.bundleManager.once('bundleChanged', bundle => {
 			expect(bundle.name).to.equal('change-manifest');
 			done();
 		});
@@ -53,43 +53,43 @@ describe('watcher', function () {
 	});
 
 	it('should remove the bundle when the manifest file is renamed', function (done) {
-		this.bundleManager.once('bundleRemoved', function () {
-			var result = this.bundleManager.find('rename-manifest');
+		this.bundleManager.once('bundleRemoved', () => {
+			const result = this.bundleManager.find('rename-manifest');
 			assert.isUndefined(result);
 			done();
-		}.bind(this));
+		});
 
 		fs.renameSync('_workingTest/bundles/rename-manifest/package.json',
 			'_workingTest/bundles/rename-manifest/package.json.renamed');
 	});
 
 	it('should emit a removed event when the manifest file is removed', function (done) {
-		this.bundleManager.once('bundleRemoved', function () {
-			var result = this.bundleManager.find('remove-manifest');
+		this.bundleManager.once('bundleRemoved', () => {
+			const result = this.bundleManager.find('remove-manifest');
 			assert.isUndefined(result);
 			done();
-		}.bind(this));
+		});
 
 		fs.unlinkSync('_workingTest/bundles/remove-manifest/package.json');
 	});
 
 	it('should emit a change event when a panel HTML file changes', function (done) {
-		this.bundleManager.once('bundleChanged', function (bundle) {
+		this.bundleManager.once('bundleChanged', bundle => {
 			expect(bundle.name).to.equal('change-panel');
 			done();
 		});
 
-		var panelPath = '_workingTest/bundles/change-panel/dashboard/panel.html';
-		var panel = fs.readFileSync(panelPath);
+		const panelPath = '_workingTest/bundles/change-panel/dashboard/panel.html';
+		let panel = fs.readFileSync(panelPath);
 		panel += '\n';
 		fs.writeFileSync(panelPath, panel);
 	});
 
 	it('should reload the bundle\'s config when the bundle is reloaded due to a change', function (done) {
-		var manifest = JSON.parse(fs.readFileSync('_workingTest/bundles/change-config/package.json'));
-		var config = JSON.parse(fs.readFileSync('_workingTest/cfg/change-config.json'));
+		const manifest = JSON.parse(fs.readFileSync('_workingTest/bundles/change-config/package.json'));
+		const config = JSON.parse(fs.readFileSync('_workingTest/cfg/change-config.json'));
 
-		this.bundleManager.once('bundleChanged', function (bundle) {
+		this.bundleManager.once('bundleChanged', bundle => {
 			expect(bundle.name).to.equal('change-config');
 			expect(bundle.config).to.deep.equal({
 				bundleConfig: true,
@@ -105,16 +105,16 @@ describe('watcher', function () {
 	});
 
 	// This has to be the last test.
-	it('should produce an unhandled exception when a panel HTML file is removed', function (done) {
+	it('should produce an unhandled exception when a panel HTML file is removed', done => {
 		// Remove Mocha's error listener
-		var originalUncaughtExceptionListeners = process.listeners('uncaughtException');
+		const originalUncaughtExceptionListeners = process.listeners('uncaughtException');
 		process.removeAllListeners('uncaughtException');
 
 		// Add our own error listener to check for unhandled exceptions
-		process.on('uncaughtException', function (err) {
+		process.on('uncaughtException', err => {
 			// Add the original error listeners again
 			process.removeAllListeners('uncaughtException');
-			for (var i = 0; i < originalUncaughtExceptionListeners.length; i += 1) {
+			for (let i = 0; i < originalUncaughtExceptionListeners.length; i += 1) {
 				process.on('uncaughtException', originalUncaughtExceptionListeners[i]);
 			}
 
@@ -127,24 +127,24 @@ describe('watcher', function () {
 	});
 });
 
-describe('per-bundle npm dependencies', function () {
-	it('should get installed', function () {
-		var dir = path.join('_workingTest/bundles/bundle-deps/node_modules/commander');
+describe('per-bundle npm dependencies', () => {
+	it('should get installed', () => {
+		const dir = path.join('_workingTest/bundles/bundle-deps/node_modules/commander');
 		expect(fs.existsSync(dir)).to.equal(true);
 	});
 });
 
-describe('per-bundle bower dependencies', function () {
-	it('should get installed', function () {
-		var dir = path.join('_workingTest/bundles/bundle-deps/bower_components/webcomponentsjs');
+describe('per-bundle bower dependencies', () => {
+	it('should get installed', () => {
+		const dir = path.join('_workingTest/bundles/bundle-deps/bower_components/webcomponentsjs');
 		expect(fs.existsSync(dir)).to.equal(true);
 	});
 });
 
 after(function (done) {
-	this.bundleManager.stopWatching();
+	this.bundleManager._stopWatching();
 	process.nextTick(() => {
-		rimraf('_workingTest', function () {
+		rimraf('_workingTest', () => {
 			done();
 		});
 	});
